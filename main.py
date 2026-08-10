89662354e715784ab6abab46c01839721cc8a28d
gunqio <vssvnj180@gmail.com>
2026-08-10 11:11:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ except ImportError:
 # ==============================================================================
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=f9ba3248-9756-48ea-aabd-a5d8ecb940e9",
-    "https://host2play.gratis/server/renew?i=d685f690-50b6-4d40-bf67-03bf072f18c0",
+    "https://host2play.gratis/server/renew?i=c02d8fbd-9752-4bfe-98c3-fc88e0ddd593",
   
 ]
 
